2d393758c4d000905ec8b0b582ed61a21c390b75
José Luiz Rosa Junior <junior.joseluizrosa@gmail.com>
2020-12-05 19:04:00 -0300

    Commit inicial

---
+++ src/main/java/br/com/atualizesistemas/apicondominio/ApiCondominioApplication.java
@@ -0,0 +1,13 @@
+package br.com.atualizesistemas.apicondominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCondominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/atualizesistemas/apicondominio/ApiCondominioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atualizesistemas.apicondominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCondominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
